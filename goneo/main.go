2588924e2988a6f807e854c27a7de9e54f8fdda6
Jonathan Buch <jonathan.buch@gmail.com>
2016-09-16 12:37:00 +0200

    Revert "goneo, add memory and cpu profiling"
    This reverts commit 2dca107d0336d32b75c105da39555f9d50883d67.

--- goneo/main.go
+++ goneo/main.go
@@ -4,56 +4,16 @@ import (
 	"flag"
 	"goneo/db/mem"
 	"goneo/web"
-	"log"
 	"math/rand"
-	"os"
-	"os/signal"
-	"runtime"
-	"runtime/pprof"
-	"syscall"
 )
 
 var (
 	binding = flag.String("bind", ":7474", "Bind to ip/port")
 	size    = flag.String("size", "small", "Size of generated graph")
-
-	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
-	memprofile = flag.String("memprofile", "", "write mem profile to file")
 )
 
 func main() {
 	flag.Parse()
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal("could not create CPU profile: ", err)
-		}
-		runtime.SetCPUProfileRate(1000)
-		if err := pprof.StartCPUProfile(f); err != nil {
-			log.Fatal("could not start CPU profile: ", err)
-		}
-		sigChan := make(chan os.Signal)
-		signal.Notify(sigChan, syscall.SIGINT)
-		go func() {
-			s := <-sigChan
-
-			if *memprofile != "" {
-				f, err := os.Create(*memprofile)
-				if err != nil {
-					log.Fatal("could not create memory profile: ", err)
-				}
-				runtime.GC() // get up-to-date statistics
-				if err := pprof.WriteHeapProfile(f); err != nil {
-					log.Fatal("could not write memory profile: ", err)
-				}
-				f.Close()
-			}
-
-			log.Printf("Recieved: %+v\n", s)
-			pprof.StopCPUProfile()
-			os.Exit(0)
-		}()
-	}
 
 	db := mem.NewDb()
 

ab26ad5c0b483efa7e34226b30016807b30f7ea0
Jonathan Buch <jonathan.buch@gmail.com>
2016-09-20 20:12:15 +0200

    filedb, implement simple in memory saving/getting nodes

--- db/file/db.go
+++ db/file/db.go
@@ -4,17 +4,34 @@ import (
 	. "github.com/BuJo/goneo/db"
 )
 
-type filedb struct{}
-
-func NewDb(db string, options map[string][]string) DatabaseService {
-	return &filedb{}
+type filedb struct {
+	nodes []*node
 }
 
-func (db *filedb) NewNode(labels ...string) Node                { return nil }
-func (db *filedb) Close()                                       {}
-func (db *filedb) GetNode(id int) (Node, error)                 { return nil, nil }
+func NewDb(name string, options map[string][]string) DatabaseService {
+	db := &filedb{}
+
+	db.nodes = make([]*node, 0)
+
+	return db
+}
+
+func (db *filedb) NewNode(labels ...string) Node {
+	n := &node{}
+
+	db.nodes = append(db.nodes, n)
+	n.id = len(db.nodes) - 1
+
+	return n
+}
+func (db *filedb) GetNode(id int) (Node, error) {
+	return db.nodes[id], nil
+}
+
 func (db *filedb) GetAllNodes() []Node                          { return nil }
 func (db *filedb) GetRelation(id int) (Relation, error)         { return nil, nil }
 func (db *filedb) GetAllRelations() []Relation                  { return nil }
 func (db *filedb) FindPath(start, end Node) Path                { return nil }
 func (db *filedb) FindNodeByProperty(prop, value string) []Node { return nil }
+
+func (db *filedb) Close() {}
--- db/file/db_test.go
+++ db/file/db_test.go
@@ -11,3 +11,18 @@ func TestOpeningAndClosingDb(t *testing.T) {
 		t.Fatal("Db should have been created")
 	}
 }
+
+func TestSavingAndGettingNodes(t *testing.T) {
+	db := NewDb("file.db", nil)
+	defer db.Close()
+
+	node := db.NewNode()
+	if node == nil {
+		t.Fatal("Node creation should work")
+	}
+
+	node, _ = db.GetNode(node.Id())
+	if node == nil {
+		t.Fatal("Node getting should work")
+	}
+}
